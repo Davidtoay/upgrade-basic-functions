1350039681e8c38faf6eea6683377d6a1f71da1e
David Torres Ayala <david.torresayala@hotmail.com>
2022-12-12 20:54:44 +0100

    Buscar el máximo

---
+++ Iteracion_1.js
@@ -0,0 +1,15 @@
+/*
+Completa la función que tomando dos números como argumento devuelva el más alto.
+*/
+
+
+function sum(numberOne, numberTwo){
+    let masAlto;
+    if(numberOne >= numberTwo){
+    }
+    else {
+    }
+    return masAlto;
+}
+console.log(sum(8, 6));
+

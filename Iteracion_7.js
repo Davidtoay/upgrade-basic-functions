28dddff88b48e2f30e43757d0e8340ac5051708a
David Torres Ayala <david.torresayala@hotmail.com>
2023-01-14 23:48:34 +0100

    Buscador de nombre

---
+++ Iteracion_7.js
@@ -0,0 +1,32 @@
+
+/*
+Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array, 
+comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento 
+y por la contra un false. Puedes usar este array para probar tu función:
+*/
+
+
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+];
+function finderName(name, lista) {
+    personas = false
+    for ( let i = 0; i < lista.length; i++){
+        if (lista[i] == name){
+        personas = true;
+        console.log(personas, i);
+        }
+    }        
+}
+
+finderName("Peter", nameFinder);
